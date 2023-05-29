c560d705c34756714dceb6cfe0a1caa7808838f6
smartful <remi_rodrigues@hotmail.com>
2023-05-30 01:58:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# MERN AUTH
+
+App in MERN stack to show how works authentication.
+
+The code come from : Traversy Media Youtube Channel
+https://www.youtube.com/watch?v=R4AhvYORZRY
---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => res.send("Server is ready"));
+
+app.listen(port, () => console.log(`Server started on port : ${port}`));
