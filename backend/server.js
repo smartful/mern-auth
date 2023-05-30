3c3edbbc23a6d9f3384cc8559ae1b52d97bc4a97
smartful <remi_rodrigues@hotmail.com>
2023-05-30 18:26:03 +0200

    setup routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 5000;
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
 
