9f57b174e808296ab5f69ea94446b96df485d061
smartful <remi_rodrigues@hotmail.com>
2023-05-30 17:48:38 +0200

    add nodemon and env variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Server is ready"));
 
