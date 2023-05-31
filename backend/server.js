2159779b42ab8172eca3c5fd9dda2a890ef439ac
smartful <remi_rodrigues@hotmail.com>
2023-05-31 18:36:03 +0200

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
+connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
