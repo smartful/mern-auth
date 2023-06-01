a20e5cd79b33198b428da82c3dbc135de19f60b1
smartful <remi_rodrigues@hotmail.com>
2023-06-01 02:21:18 +0200

    creation of user model + implementation of user register

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server is ready"));
 
