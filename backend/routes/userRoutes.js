7c3b51d92badde928a9b4e2a469b5810c7f1d41b
smartful <remi_rodrigues@hotmail.com>
2023-06-01 19:01:09 +0200

    creation of auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,12 +6,15 @@ import {
   getUserProfile,
   updateUserProfile,
 } from "../controllers/userController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.get("/profile", getUserProfile);
-router.put("/profile", updateUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server is ready"));
