334ec93480ad73649574ffaee89580d8eeefb0ac
smartful <remi_rodrigues@hotmail.com>
2023-06-05 18:49:23 +0200

    creation of Login Screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
