422bc0cb486b6ca78a0c2fe62267cbf67b901c51
smartful <remi_rodrigues@hotmail.com>
2023-06-05 05:29:38 +0200

    Router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
-
 const App = () => {
   return (
     <>
       <Header />
-      <HomeScreen />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   );
 };
