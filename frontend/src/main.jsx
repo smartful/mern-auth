5bb9251c433ce15b8e41d7b8f6c4bea2fe4435a4
smartful <remi_rodrigues@hotmail.com>
2023-06-06 18:15:31 +0200

    Protect some screens with Private Route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,9 +9,11 @@ import {
 import store from "./store.js";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
@@ -21,6 +23,9 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 );
